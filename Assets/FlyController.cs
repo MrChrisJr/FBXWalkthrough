d3323911b23e3914df7ef13a635bf6dfa3ee7f28
MrChrisJr <mrchrisjr1024@gmail.com>
2023-05-02 20:48:15 -0700

    Camera and House switching code added!

--- Assets/FlyController.cs
+++ Assets/FlyController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,47 @@ using TMPro;
 public class FlyController : MonoBehaviour
 {
 
-    public Camera playerCamera;
-    public Object building1;
-    public Object building2;
-    public Object building3;
+    private Camera playerCamera;
+    [SerializeField] private GameObject[] buildings = new GameObject[3];
+    [SerializeField] private GameObject[] floorSys = new GameObject[3];
     public TextMeshProUGUI Press;
 
+    private void Start()
+    {
         
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            LoadBuilding(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            LoadBuilding(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            LoadBuilding(2);
+        }
+    }
+
+    private void LoadBuilding(int building) // 0 - 2
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            if(i == building)
+            {
+                buildings[i].SetActive(true);
+                floorSys[i].SetActive(true);
+            }
+            else
+            {
+                buildings[i].SetActive(false);
+                floorSys[i].SetActive(false);
+            }
+        }
+    }
+
 }
